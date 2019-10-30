d3696f81aebcc47da7c0087ed9842d5d66ab25a9
Andy Carluccio <45571628+AndrewCarluccio@users.noreply.github.com>
2019-10-30 18:10:12 -0400

    Update accel/decel
    WIP

---
+++ MiniBot/Firmware/MINIBOT_FIRMWARE/MINIBOT_FIRMWARE.ino
@@ -0,0 +1,264 @@
+#include <Boards.h>
+#include <Firmata.h>
+#include <FirmataConstants.h>
+#include <FirmataDefines.h>
+#include <FirmataMarshaller.h>
+#include <FirmataParser.h>
+
+//FIRMWARE FOR UVA MINI STAGE ROBOT
+
+//Software Serial Setup------------------------------------
+#include <SoftwareSerial.h>
+SoftwareSerial SWSerial(8,5); // Available software serial pins
+
+//Motor Controller-------------------------
+#include <Sabertooth.h>
+
+//KANG---------
+#include <Kangaroo.h>
+KangarooSerial  K(SWSerial);
+KangarooChannel K1(K, 'T');
+KangarooChannel K2(K, 'D');
+
+long turnReading = 0;
+long driveReading = 0;
+
+//I/O----------------------------------------
+int brake = 3;            //controls motor brakes
+int ledR = 11;             //common anode LED high=off low=on
+int ledG = 10;
+int ledB = 9;
+
+int buttonPin = 12;
+
+// Motor Global Variables--------------------------
+
+float commandCode = 0.0;
+float packetSize = 0.0;
+bool buttonPressed = false;
+bool driveFlag = false;
+bool turnFlag = false;
+
+int destination = 0;
+int velocity = 0;
+int rotation = 0;
+
+int prev_velocity = 0;
+//Linux Support ---------------------------------- 
+#include <Bridge.h>
+#include <stdio.h>
+#include <Process.h>
+
+Process p;
+ 
+void setup() {
+  //Serial configuration
+  Serial.begin(115200);
+  //while(!Serial); //comment this line to remove the pause
+  Serial.println("starting setup...");
+  
+  SWSerial.begin(9600);             //start sabertooth with software serial
+  Sabertooth::autobaud(SWSerial);
+
+  
+  //LED----------------------
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  pinMode(ledB, OUTPUT);
+
+  digitalWrite(ledR, LOW);   //turn LEDs RED on startup
+  digitalWrite(ledG, HIGH);
+  digitalWrite(ledB, HIGH);
+
+  
+  //KANG------
+  K1.start();
+  K2.start();
+
+  K1.s(0);
+  K2.s(0);
+
+  //K2.units(1, (34/2.559*3.14159));
+
+  Serial.println("Finished KANG Setup");
+
+  // Misc -------------------
+  pinMode(buttonPin, INPUT);
+  
+  // Start using the Bridge and Python script
+  Serial.println("Starting bridge...");
+  Bridge.begin();
+
+  Serial.println("running python script...");
+  p.runShellCommandAsynchronously("python -u /usr/lib/andyCode/COMPLEX_CUE_MIDDLEWARE.py");
+  Serial.println("command sent!");
+
+  Serial.println("waiting for data influx...");
+  digitalWrite(ledG, LOW); //turn LEDs YELLOW while waiting
+  while(!p.available());
+
+  digitalWrite(ledR, HIGH);   //turn LEDs GREEN when leaving setup
+  Serial.println("setup complete");
+}
+
+/*
+ * WHAT THE LOOP DOES
+ * 
+ * First, check the eStopStatus
+ * If not in eStop scenerio, check Bridge for a command code
+ * Run the proper subroutine for the active command code
+ * 
+ */
+void loop() {
+    updateCMDCode();
+    
+    //trigger subroutines based on the type of mode we are in
+    if(commandCode==1.0){ //enter load mode
+      
+    }
+    else if(commandCode==2.0){  //exit load mode
+      
+    }
+    else if(commandCode==3.0){  //manual control mode
+      //manualMode();
+    }
+    else if(commandCode==4.0){  //cue control mode
+      cueControlMode();
+    }
+    else if(commandCode==5.0){  //soft stop trigger mode
+      
+    }
+}
+
+
+void readEncoders() {
+  //encoder1Reading = Encoder1.readEncoder();         //Read Encoder
+  //encoder2Reading = Encoder2.readEncoder();           //Read Encoder
+  //Encoder1.clearEncoderCount();                 // Clear Encoder
+  turnReading = K1.getP().value();
+  driveReading = K2.getP().value();
+}
+
+//-------------------Get an int off the Bridge-----------------------------------------------
+float getNextNum(){
+  float res = -1;
+  if(p.available()){
+    res = p.parseInt()/100.0; //offset back to +/- range
+  }
+  return res;
+}
+
+//----------------------Update Size and CMD Code---------------------------------------------
+bool updateCMDCode(){
+  //Serial.println("Running update");
+  if(p.available()){
+    packetSize = p.parseInt()/100.0;
+  }
+  if(packetSize==1){ //command packets have length 1 after size
+    if(p.available()){
+      commandCode = p.parseInt()/100.0;
+      Serial.println("updated command code");
+      return true;
+    }
+  }
+  return false;
+}
+
+//-------------------CONTROL MODE SUBROUTINES---------------------------------------------
+
+void cueControlMode(){
+  readEncoders();
+  int metadata = 0;
+  if(digitalRead(buttonPin)==HIGH){
+
+    p.println(-1);
+    
+    driveFlag = true;
+    turnFlag = false;
+    while(!p.available()){Serial.print("waiting...");}
+    getNextNum(); //flush the length out
+    destination = getNextNum();
+    velocity = getNextNum();
+    velocity = 0; //doing a dumb thing here
+    rotation = getNextNum();
+    
+    Serial.print("Destination is: ");
+    Serial.print(destination);
+    Serial.println("ticks");
+
+    p.println(driveReading);
+    
+    //K2.s(velocity);
+    K2.p(destination,velocity);
+    digitalWrite(ledR, HIGH);  
+    digitalWrite(ledG, HIGH);
+    digitalWrite(ledB, HIGH);
+  }
+  if(driveFlag){
+    p.println(driveReading);
+    Serial.print("Encoder position: ");
+    Serial.println(driveReading);
+    while(!p.available()){Serial.print("waiting...");}
+    getNextNum(); //flush size bit
+    metadata = getNextNum();
+    Serial.print("metadata: ");
+    Serial.println(metadata);
+    velocity = getNextNum();
+    Serial.print("velocity received is: ");
+    Serial.println(velocity);
+    if(velocity>0){
+      K2.s(velocity);
+      if(velocity>prev_velocity){
+        digitalWrite(ledR, LOW);  
+        digitalWrite(ledG, HIGH);
+        digitalWrite(ledB, HIGH);
+        Serial.println("Accelerating!");
+        //Serial.print("Velocity: ");
+        //Serial.println(velocity);
+      }
+      else if(velocity==prev_velocity){
+        digitalWrite(ledR, HIGH);  
+        digitalWrite(ledG, LOW);
+        digitalWrite(ledB, HIGH);
+        Serial.println("Crusing!");
+        //Serial.print("Velocity: ");
+        //Serial.println(velocity);
+      }
+      else{
+        digitalWrite(ledR, HIGH);  
+        digitalWrite(ledG, HIGH);
+        digitalWrite(ledB, LOW);
+        Serial.println("Decelerating!");
+        //Serial.print("Velocity: ");
+        //Serial.println(velocity);
+      }
+    }
+    prev_velocity = velocity;
+  }
+  if(driveReading>=destination){
+    driveFlag = false;
+    turnFlag = true;
+    K2.s(0);
+    //K1.p(rotation,0);
+    p.println(-2); //we finished the drive, are now turning
+  }
+  /*
+  if(turnFlag){
+    p.println(turnReading);
+    metadata = getNextNum();
+    velocity = getNextNum();
+    if(velocity>0){
+      K1.s(velocity);
+    }
+  }
+  if(turnReading>=rotation){
+    turnFlag = false;
+    p.println(-3); //we finished the turn, all done!
+  }
+  */
+}
+
+/*
+ * CODE GRAVEYARD
+ * IN HONOR OF TIM
+ */
--- MiniBot/Middleware/COMPLEX_CUE_MIDDLEWARE.py
+++ MiniBot/Middleware/COMPLEX_CUE_MIDDLEWARE.py
@@ -21,26 +21,48 @@ Good things to know about communication with the Arduino environment:
 #setup------------------------
 import sys
 import os
-import requests
+#import requests
 import math
-sys.path.insert(0, '/usr/lib/python2.7/bridge')
+#sys.path.insert(0, '/usr/lib/python2.7/bridge')
 from time import sleep
-from bridgeclient import BridgeClient as bridgeclient
-value = bridgeclient()
+#from bridgeclient import BridgeClient as bridgeclient
+#value = bridgeclient()
 
-dist = 5 #in feet
+dist = 60 #in inches
+rot = 90 #in degrees
+acceleration = 3 #inches per second^2
+deceleration = -8
+velocity = 12 #inches per second
+
+driveENC = 0
+
+acc_end = 0
+dec_beg = 0
+endpt = 0
+
+live_vel = 0
+
+acc_metadata = 3
+cruise_metadata = 4
+dec_metadata = 5
+
+drive_flag = False
+turn_flag = False
+
+unit_tick = 1
+line_tick = 34/2.559*math.pi
 
 def sendCommand(cmd):
     chars = []
 
     #offset values to positive range
     for instr in cmd:
-        chars.append(int(instr))
+        chars.append(int((float(instr))*100))
 
     #the console is what the Ardunio Processor reads from, must flush each time
 
     #the first message must always be the number of messages to follow
-    print(len(chars))
+    print(len(chars)*100)
     sys.stdout.flush()
 
     for msg in chars:
@@ -51,28 +73,33 @@ def getData():
     inputA = sys.stdin.readline()
     #inputA = inputA.rstrip('\n')
     #inputA = inputA.strip()
-    inputA = int(inputA)
+    inputA = float(inputA)
 
     return inputA
+
+def precalcDest(loc):
+    return loc+dist
+
+def getAccelVel(accel, vel_now, enc_now, point):
+    new_vel = math.sqrt(vel_now**2.0 + 2.0*accel*(point-enc_now)/point)
+    #new_vel = vel_now+1
+    return new_vel
+
+def getDecVel(accel, vel_now, enc_now, point):
+    new_vel = math.sqrt(vel_now**2.0 + 2.0*accel*(((dist-point)-(enc_now-point))/(dist-point)))
+    #new_vel = vel_now+1
+    return new_vel
+
+def convertToLineUnit(units):
+    return ((units*374)/(2.559*math.pi))
+
+def convertToCustomUnit(line_units):
+    return (line_units*2.559*math.pi/374)
 
 pinFlag = False
 
 
 sendCommand("4")
-encoder1 = 0
-encoder2 = 0
-
-encoderFinish1 = 0
-encoderFinish2 = 0
-
-#make sure to multiply by pi
-def preCalcEnc(enc,feet):
-    inches = feet*12
-
-   # endLoc = 2.5*3.14159*inches*2400+enc
-    endLoc = ((inches*2400)/(2.5*3.14159)) +enc
-
-    return endLoc
 
 while(True):
     if(not pinFlag):
@@ -81,17 +108,79 @@ while(True):
     #if go pin was pressed
     if(arduinoReading==-1):
         pinFlag = True
-        encoder1 = getData()
-        encoder2 = getData()
-        encoderFinish1 = preCalcEnc(encoder1,dist)
-        encoderFinish2 = preCalcEnc(encoder2,dist)
+        drive_flag = True
+        sendCommand([convertToLineUnit(dist),convertToLineUnit(velocity),convertToLineUnit(rot)])
+        driveENC = convertToCustomUnit(getData())
+        endpt = precalcDest(driveENC)
+
+        accTime = velocity / acceleration
+        acc_end = (velocity/2) * accTime 
+
+        decTime = velocity / deceleration*-1
+        dec_beg = dist - ((velocity/2) * decTime) #mult by 2 could be an issue...
         arduinoReading=0
 
     if(pinFlag):
-        encoder1 = getData()
-        encoder2 = getData()
-        if(encoder1<encoderFinish1 and encoder2<encoderFinish2):
-            sendCommand([127,127]) #tells the robot to go full speed forward
+        driveENC = convertToCustomUnit(getData())
+        if(driveENC<0): #indicates that a command was passed, not an encoder!
+            if(driveENC==-2): #completed the drive
+                drive_flag = False
+                turn_flag = True
+                #update the acc and dec with respect to the rotation (THIS IS  A PROBLEM! NO UNIQUE VEL CURVES FOR ROT)
+                accTime = velocity / acceleration
+                acc_end = (velocity/2) * accTime
+                decTime = (-1 * velocity) / deceleration
+                dec_beg = rot - ((velocity/2) * decTime)
+                live_vel = 0.1
+                prev_driveENC = 0
+            elif(driveENC==-3): #completed the turn
+                turn_flag = False
+                pinFlag = False
+
+        #Send velocities for a linear drive
+        elif(drive_flag and driveENC<acc_end):
+            #accelerate
+            live_vel = getAccelVel(acceleration, live_vel, driveENC, acc_end)
+            sendCommand([acc_metadata,convertToLineUnit(live_vel)])
+            prev_driveENC = driveENC
+        elif(drive_flag and driveENC>acc_end and driveENC<dec_beg):
+            #cruise
+            sendCommand([cruise_metadata, convertToLineUnit(velocity)])
+            prev_driveENC = driveENC
+            live_vel=velocity
+        elif(drive_flag and driveENC>dec_beg and driveENC<dist):
+            #deccelerate 
+            live_vel = getDecVel(deceleration, live_vel, driveENC, dec_beg)
+            sendCommand([dec_metadata,convertToLineUnit(live_vel)])
+            prev_driveENC = driveENC
+        elif(drive_flag and driveENC>dist):
+            velocity=0
+            sendCommand([dec_metadata, convertToLineUnit(velocity)])
+            live_vel=velocity
+            prev_driveENC = driveENC
+
+
+        #Send velocities for a turn
+        elif(turn_flag and driveENC<acc_end):
+            #accelerate
+            live_vel = getAccelVel(acceleration, live_vel, driveENC, acc_end)
+            sendCommand([acc_metadata,convertToLineUnit(live_vel)])
+            prev_driveENC = driveENC
+        elif(turn_flag and driveENC>acc_end and driveENC<dec_beg):
+            #cruise
+            sendCommand([cruise_metadata, convertToLineUnit(velocity)])
+            prev_driveENC = driveENC
+            live_vel=velocity
+        elif(drive_flag and driveENC>dec_beg and driveENC<dist):
+            #deccelerate 
+            live_vel = getDecVel(deceleration, live_vel, driveENC, dec_beg)
+            sendCommand([dec_metadata,convertToLineUnit(live_vel)])
+            prev_driveENC = driveENC
         else:
-            sendCommand([0,0]) #stop the robot
-            pinFlag = False
+            velocity=0
+            sendCommand([dec_metadata, convertToLineUnit(velocity)])
+            live_vel=velocity
+            prev_driveENC = driveENC
+
+
+
